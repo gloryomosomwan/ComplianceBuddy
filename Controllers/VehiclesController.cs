dc215e1185065039a8ff5439982163e54dbb7ec2
Glory Omosomwan <80617508+asvpglory@users.noreply.github.com>
2025-11-26 20:18:19 -0700

    add vehicles from button

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -23,6 +23,12 @@ namespace ComplianceBuddy.Controllers
       return View();
     }
 
+    public IActionResult Lmao()
+    {
+      Console.WriteLine("Helo!");
+      return RedirectToAction("Create");
+    }
+
     [HttpPost]
     public async Task<IActionResult> Create(Vehicle vehicle)
     {
