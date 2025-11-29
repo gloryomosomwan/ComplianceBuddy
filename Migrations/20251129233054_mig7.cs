4bd541e733953f9854262c3e0d2a4b60cdd4f7ef
Glory Omosomwan <80617508+asvpglory@users.noreply.github.com>
2025-11-29 16:31:24 -0700

    chore: add migration

---
+++ Migrations/20251129233054_mig7.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ComplianceBuddy.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig7 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "VehicleId",
+                table: "Inspections",
+                newName: "Vin");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Vin",
+                table: "Inspections",
+                newName: "VehicleId");
+        }
+    }
+}
