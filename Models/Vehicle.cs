1cf67a0eaab020e4823c52a9fde0459b388b2d01
Glory Omosomwan <80617508+asvpglory@users.noreply.github.com>
2025-12-02 00:21:21 -0700

    feat(vehicle): add form validation for vehicle

--- Models/Vehicle.cs
+++ Models/Vehicle.cs
@@ -5,13 +5,19 @@ namespace ComplianceBuddy.Models
   public class Vehicle
   {
     public int Id { get; set; }
-    [Required]
+
+    [Required(ErrorMessage = "VIN is required.")]
+    [StringLength(17, MinimumLength = 17, ErrorMessage = "VIN must be exactly 17 characters.")]
     public string Vin { get; set; } = null!;
-    [Required]
+
+    [Required(ErrorMessage = "Vehicle make is required.")]
     public string Make { get; set; } = null!;
-    [Required]
+
+    [Required(ErrorMessage = "Vehicle model is required.")]
     public string Model { get; set; } = null!;
-    [Required]
+
+    [Required(ErrorMessage = "Vehicle year is required.")]
+    [Range(1900, 2025, ErrorMessage = "Please enter a valid year between 1900 and 2025.")]
     public int Year { get; set; }
 
     public string UserId { get; set; } = null!;
